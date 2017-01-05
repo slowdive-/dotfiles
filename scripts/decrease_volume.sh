34efec8f57045142d5fb28ddd77e4aadef97fa0d
slowdive- <slowdive-@github.com>
2017-01-05 14:06:59 +0100

    Volume 0 mutet amixer

---
+++ scripts/decrease_volume.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+amixer -q set Master 5%-
+
+# Mute if volume < 1
+vol=$(expr $(amixer get Master | grep % | cut -d'[' -f 2 | cut -d']' -f 1 | grep -o "[0-9]*"))
+if [ $vol -lt 1 ]
+then
+	amixer -q set Master mute
+fi
+
