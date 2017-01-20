6f98fa7d330e4a15920ea7d8f0f789f1d2a5082e
slowdive- <slowdive-@github.com>
2017-01-20 17:44:36 +0100

    slideshow ist optional

--- scripts/bg-script.sh
+++ scripts/bg-script.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-while [ 1 ]
-do
-	for image in ~/wallpapers/*.*
+if [ "$1" = "-s" ] || [ "$1" = "--slideshow" ]
+then
+	while [ 1 ]
 	do
-		feh  --bg-scale $image --no-fehbg
-		sleep 120
+		for image in ~/wallpapers/*.*
+		do
+			feh  --bg-scale $image --no-fehbg
+			sleep 120
+		done
 	done
-done
+else
+	feh  --bg-scale ~/wallpapers/default.png --no-fehbg
+fi
