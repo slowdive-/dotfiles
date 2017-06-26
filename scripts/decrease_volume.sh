c6e47fd63f7f45150b7ab6f8b15264309c0e2e66
slowdive- <slowdive-@github.com>
2017-06-26 18:40:49 +0200

    Volume Skripte auf Pulseaudio angepasst

--- scripts/decrease_volume.sh
+++ scripts/decrease_volume.sh
@@ -2,9 +2,8 @@
 amixer -q set Master 5%-
 
 # Mute if volume < 1
-vol=$(expr $(amixer get Master | grep % | cut -d'[' -f 2 | cut -d']' -f 1 | grep -o "[0-9]*"))
+vol=$(expr $(amixer get Master | grep -e "Left.*%" | cut -d'[' -f 2 | cut -d']' -f 1 | grep -o "[0-9]*"))
 if [ $vol -lt 1 ]
 then
 	amixer -q set Master mute
 fi
-
